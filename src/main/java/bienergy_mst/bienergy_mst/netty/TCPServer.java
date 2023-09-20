f6119a0269a3b0e6f58b534e2599301d981c376d
skam12345 <skam1234561@gmail.com>
2023-09-20 13:33:08 +0900

    포트변경

--- src/main/java/bienergy_mst/bienergy_mst/netty/TCPServer.java
+++ src/main/java/bienergy_mst/bienergy_mst/netty/TCPServer.java
@@ -1,47 +1,3 @@
-// package bienergy_mst.bienergy_mst.netty;
-
-// import java.net.InetSocketAddress;
-
-// import javax.annotation.PreDestroy;
-
-// import org.springframework.stereotype.Component;
-
-// import io.netty.bootstrap.ServerBootstrap;
-// import io.netty.channel.Channel;
-// import io.netty.channel.ChannelFuture;
-// import lombok.RequiredArgsConstructor;
-
-// @RequiredArgsConstructor 
-// @Component
-// public class TCPServer {
-    
-//     private final ServerBootstrap serverBootstrap;
-    
-//     private final InetSocketAddress tcpPort;
-    
-//     private Channel serverChannel;
-
-//     public void start() {
-//         try {
-//             ChannelFuture serverChannelFuture = serverBootstrap.bind(tcpPort).sync();      		
-//             System.out.println(tcpPort.getPort());
-//     		serverChannel = serverChannelFuture.channel().closeFuture().sync().channel();
-//         } catch(InterruptedException e) {
-//             Thread.currentThread().interrupt();
-//         }
-//     }
-    
-    
-
-//     @PreDestroy
-//     public void stop() {
-//         if( serverChannel != null ) {
-//             serverChannel.close();
-//             serverChannel.parent().close();
-//         }
-//     }
-// }
-
 package bienergy_mst.bienergy_mst.netty;
 
 import java.net.InetSocketAddress;
@@ -67,7 +23,7 @@ public class TCPServer {
 
     public void start() {
         try {
-            ChannelFuture serverChannelFuture = serverBootstrap.bind(7888).sync();      		
+            ChannelFuture serverChannelFuture = serverBootstrap.bind(tcpPort).sync();      		
             System.out.println(tcpPort.getPort());
     		serverChannel = serverChannelFuture.channel().closeFuture().sync().channel();
         } catch(InterruptedException e) {
@@ -85,4 +41,3 @@ public class TCPServer {
         }
     }
 }
-
