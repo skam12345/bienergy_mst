9de13996d4af6506e1286f8282a7549f9e99a3cb
skam12345 <skam1234561@gmail.com>
2023-09-22 10:39:28 +0900

    code modify 3

--- src/main/java/bienergy_mst/bienergy_mst/netty/handler/ControlPop.java
+++ src/main/java/bienergy_mst/bienergy_mst/netty/handler/ControlPop.java
@@ -15,61 +15,61 @@ public class ControlPop {
 		
 	}
 	public void run(Connection conn, QueryExcuteClass execute, ChannelPipeline pipline, LoginHandler loginHandler, ControlHandler control, int count) throws Exception {
-		ExecutorService service = Executors.newFixedThreadPool(1);
-		service.execute(() -> {
-			try {
-				if(loginHandler.getCopy() != null) {
-					while(true) {
+		if(loginHandler.getCopy() != null) {
+			ExecutorService service = Executors.newFixedThreadPool(1);
+			service.execute(() -> {
+				while(true) {
+					try {
 						Thread.sleep(4000);
-						int sendNo = execute.callCommand(loginHandler.getCopy().substring(0, 12));
-						ChargerDataModel model = execute.callChargerData(sendNo);
-						String code = execute.checkCommand(loginHandler.getCopy().substring(0, 12), model.getPlugNo());
-						if(code != null) {
-							if(code.equals("0AH02")) {
-								List<Integer> sendNoList = execute.callStartCommand(loginHandler.getCopy().substring(0, 12));
-								System.out.println("충전 시작합니다.");	
-								control.setCode(code);
-								loginHandler.setModel(model);
-								control.setChargerDataModel(model);
-								control.setValues(loginHandler.getCopy());
-								System.out.println("명령을 대기합니다.");
-							}
-							if(code.equals("0AH03")) {
-								List<Integer> sendNoList = execute.callStopCommand(loginHandler.getCopy().substring(0, 12));
-								System.out.println("충전을 중지합니다.");
-								control.setCode(code);
-								loginHandler.setModel(model);
-								control.setChargerDataModel(model);
-								control.setValues(loginHandler.getCopy());
-								control.setPipline(pipline);
-							}    						
-						}else {
-							continue;
-						}
+					}catch(Exception e) {
+						e.printStackTrace();
 					}
-				}else {
-					System.out.printf("잠시만 기다려주세요.\n충전기로부터 서버와의 로그인 대기중 입니다.");
-					ExecutorService loginCheckThread = Executors.newFixedThreadPool(1);
-					loginCheckThread.execute(() -> {
-						try {
-							while(loginHandler.getCopy() != null) {
-								Thread.sleep(4000);
-							}
-							System.out.println("서버와 충전기가 로그인 되었습니다.");
-							System.out.println("명령 대기중...");
-							run(conn, execute, pipline, loginHandler, control, count);
-							loginCheckThread.shutdown();
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+
+					int sendNo = execute.callCommand(loginHandler.getCopy().substring(0, 12));
+					ChargerDataModel model = execute.callChargerData(sendNo);
+					String code = execute.checkCommand(loginHandler.getCopy().substring(0, 12), model.getPlugNo());
+					if(code != null) {
+						if(code.equals("0AH02")) {
+							List<Integer> sendNoList = execute.callStartCommand(loginHandler.getCopy().substring(0, 12));
+							System.out.println("충전 시작합니다.");	
+							control.setCode(code);
+							loginHandler.setModel(model);
+							control.setChargerDataModel(model);
+							control.setValues(loginHandler.getCopy());
+							System.out.println("명령을 대기합니다.");
 						}
-					}); 
-					Thread.sleep(1000);
+						if(code.equals("0AH03")) {
+							List<Integer> sendNoList = execute.callStopCommand(loginHandler.getCopy().substring(0, 12));
+							System.out.println("충전을 중지합니다.");
+							control.setCode(code);
+							loginHandler.setModel(model);
+							control.setChargerDataModel(model);
+							control.setValues(loginHandler.getCopy());
+							control.setPipline(pipline);
+						}    						
+					}else {
+						continue;
+					}
 				}
-			}catch(Exception e) {
-				e.printStackTrace();
-			
-			}
-		});
+			});
+		}else {
+			System.out.printf("잠시만 기다려주세요.\n충전기로부터 서버와의 로그인 대기중 입니다.");
+			ExecutorService loginCheckThread = Executors.newFixedThreadPool(1);
+			loginCheckThread.execute(() -> {
+				try {
+					while(loginHandler.getCopy() != null) {
+						Thread.sleep(4000);
+					}
+					System.out.println("서버와 충전기가 로그인 되었습니다.");
+					System.out.println("명령 대기중...");
+					run(conn, execute, pipline, loginHandler, control, count);
+					loginCheckThread.shutdown();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}); 
+			Thread.sleep(1000);
+		}
     }
 }
